a6cc77510efc37a91115f7e8c5c03fd7272f03cc
EkaterinaBildina <kukresch@gmail.com>
2023-06-08 21:41:40 +0300

    OOP05 Add Validation for Name and LastName

--- src/personal/views/ViewUser.java
+++ src/personal/views/ViewUser.java
@@ -2,6 +2,7 @@ package personal.views;
 
 import personal.controllers.UserController;
 import personal.model.User;
+import personal.views.validator.NameValidator;
 
 import java.util.List;
 import java.util.Scanner;
@@ -70,14 +71,16 @@ public class ViewUser {
         return user;
     }
 
-    private void createUser() {
+    private void createUser() throws Exception {
         User user = getNewUser();
         userController.saveUser(user);
     }
 
-    private User getNewUser() {
+    private User getNewUser() throws Exception {
         String firstName = prompt("Имя: ");
+        new NameValidator(firstName).validate();
         String lastName = prompt("Фамилия: ");
+        new NameValidator(lastName).validate();
         String phone = prompt("Номер телефона: ");
         User user = new User(firstName, lastName, phone);
         return user;
